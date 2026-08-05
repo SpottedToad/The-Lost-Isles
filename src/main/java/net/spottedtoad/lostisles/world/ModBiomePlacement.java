bdbc94802c60479068ddb983d561f50d72cd97f2
SpottedToad <deepspacekind@gmail.com>
2026-08-04 22:06:17 -0600

    working on fixing stuff

--- src/main/java/net/spottedtoad/lostisles/world/ModBiomePlacement.java
+++ src/main/java/net/spottedtoad/lostisles/world/ModBiomePlacement.java
@@ -4,36 +4,32 @@ import com.terraformersmc.biolith.api.biome.BiomePlacement;
 import com.terraformersmc.biolith.api.biome.sub.Criterion;
 import com.terraformersmc.biolith.api.biome.sub.CriterionBuilder;
 import com.terraformersmc.biolith.api.biome.sub.RatioTargets;
-import net.minecraft.world.level.biome.Climate;
+import net.minecraft.world.level.biome.Biomes;
 import net.spottedtoad.lostisles.TheLostIsles;
 import net.spottedtoad.lostisles.world.biomes.ModBiomes;
 
+import static com.terraformersmc.biolith.api.biome.sub.CriterionBuilder.ratio;
+
 public class ModBiomePlacement {
     public static void init() {
-        Climate.ParameterPoint islandBuilder = Climate.parameters(
-                Climate.Parameter.span(0.4F, 1.0F), // Temperature
-                Climate.Parameter.span(-1.0F, 1.0F), // Humidity
-                Climate.Parameter.span(-1.25F, -0.455F), // Continentalness
-                Climate.Parameter.span(-1.0F, 1.0F), // Erosion
-                Climate.Parameter.span(0.0F, 0.0F),  // Depth
-                Climate.Parameter.span(-0.1F, 0.2F), // Weirdness
-                0L // Offset (Artificially increase rarity)
+        //Replace 100% of DEEP_OCEAN with DEEP_PRIMORDIAL_OCEAN
+        BiomePlacement.replaceOverworld(
+                Biomes.DEEP_OCEAN,
+                ModBiomes.DEEP_PRIMORDIAL_OCEAN,
+                1.0D
         );
-        //Creates PRIMORDIAL_ISLAND
-        BiomePlacement.addOverworld(ModBiomes.PRIMORDIAL_ISLAND, islandBuilder);
-        //Fills 70% of the outer area of the PRIMORDIAL_ISLAND with PRIMORDIAL_OCEAN
-        Criterion shallowOceanRing = CriterionBuilder.ratioMax(RatioTargets.EDGE, 0.70F);
+        //Fills the area between 25% and 80% from the edge of the DEEP_PRIMORDIAL_OCEAN with PRIMORDIAL_OCEAN
+        Criterion shallowOceanRing = CriterionBuilder.ratio(RatioTargets.EDGE, 0.25F, 0.80F);
         BiomePlacement.addSubOverworld(
-                ModBiomes.PRIMORDIAL_ISLAND,
+                ModBiomes.DEEP_PRIMORDIAL_OCEAN,
                 ModBiomes.PRIMORDIAL_OCEAN,
                 shallowOceanRing
         );
-        //Fills 80% of the outer area of the PRIMORDIAL_OCEAN with DEEP_PRIMORDIAL_OCEAN
-        //Which is roughly 56% of the outer area of the PRIMORDIAL_ISLAND
-        Criterion deepOceanRing = CriterionBuilder.ratioMax(RatioTargets.EDGE, 0.80F);
+        //Fills the area between 80% and 100% from the edge of the PRIMORDIAL_OCEAN with PRIMORDIAL_ISLAND
+        Criterion deepOceanRing = CriterionBuilder.ratio(RatioTargets.EDGE, 0.80F, 1F);
         BiomePlacement.addSubOverworld(
                 ModBiomes.PRIMORDIAL_OCEAN,
-                ModBiomes.DEEP_PRIMORDIAL_OCEAN,
+                ModBiomes.PRIMORDIAL_ISLAND,
                 deepOceanRing
         );
     }
