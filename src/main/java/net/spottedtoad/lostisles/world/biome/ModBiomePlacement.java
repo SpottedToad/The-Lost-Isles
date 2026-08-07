cf8218fdfd6e6a2ede410ac38403989376cda8dc
SpottedToad <deepspacekind@gmail.com>
2026-08-07 14:02:03 -0600

    accidentally removed wood api dependency in gradle properties. fixed that. also did other stuff

--- src/main/java/net/spottedtoad/lostisles/world/biome/ModBiomePlacement.java
+++ src/main/java/net/spottedtoad/lostisles/world/biome/ModBiomePlacement.java
@@ -1,9 +1,6 @@
 package net.spottedtoad.lostisles.world.biome;
 
 import com.terraformersmc.biolith.api.biome.BiomePlacement;
-import com.terraformersmc.biolith.api.biome.sub.Criterion;
-import com.terraformersmc.biolith.api.biome.sub.CriterionBuilder;
-import com.terraformersmc.biolith.api.biome.sub.RatioTargets;
 import net.minecraft.world.level.biome.Biomes;
 import net.spottedtoad.lostisles.TheLostIsles;
 
@@ -12,24 +9,8 @@ public class ModBiomePlacement {
         //Replaces 100% of DEEP_LUKEWARM_OCEAN biomes with the DEEP_PRIMORDIAL_OCEAN biome
         BiomePlacement.replaceOverworld(
                 Biomes.DEEP_LUKEWARM_OCEAN,
-                ModBiomes.DEEP_PRIMORDIAL_OCEAN,
+                ModBiomes.DEEP_STORMY_LUKEWARM_OCEAN,
                 1.0F
-        );
-
-        //Fills the area between 0% and 25% from the center of the DEEP_PRIMORDIAL_OCEAN with PRIMORDIAL_ISLAND
-        Criterion islandCenter = CriterionBuilder.ratio(RatioTargets.CENTER, 0.0F, 0.25F);
-        BiomePlacement.addSubOverworld(
-                ModBiomes.DEEP_PRIMORDIAL_OCEAN,
-                ModBiomes.PRIMORDIAL_ISLAND,
-                islandCenter
-        );
-
-        //Fills the area between 25% and 50% from the center of the DEEP_PRIMORDIAL_OCEAN with PRIMORDIAL_OCEAN
-        Criterion shallowOceanRing = CriterionBuilder.ratio(RatioTargets.CENTER, 0.25F, 0.50F);
-        BiomePlacement.addSubOverworld(
-                ModBiomes.DEEP_PRIMORDIAL_OCEAN,
-                ModBiomes.PRIMORDIAL_OCEAN,
-                shallowOceanRing
         );
     }
 
--- src/main/java/net/spottedtoad/lostisles/world/biome/ModBiomes.java
+++ src/main/java/net/spottedtoad/lostisles/world/biome/ModBiomes.java
@@ -3,13 +3,19 @@ package net.spottedtoad.lostisles.world.biome;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BiomeDefaultFeatures;
 import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.data.worldgen.placement.AquaticPlacements;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.BiomeGenerationSettings;
 import net.minecraft.world.level.biome.BiomeSpecialEffects;
 import net.minecraft.world.level.biome.MobSpawnSettings;
+import net.minecraft.world.level.levelgen.GenerationStep;
 import net.spottedtoad.lostisles.TheLostIsles;
+
+import static net.minecraft.data.worldgen.BiomeDefaultFeatures.commonSpawns;
 
 public class ModBiomes {
     private static void register(BootstrapContext<Biome> context, ResourceKey<Biome> key, Biome biome) {
@@ -17,33 +23,40 @@ public class ModBiomes {
     private static ResourceKey<Biome> registerBiomeKey(String name) {
         return ResourceKey.create(Registries.BIOME, Identifier.fromNamespaceAndPath(TheLostIsles.MOD_ID, name));}
 
+    public static final ResourceKey<Biome> DEEP_STORMY_LUKEWARM_OCEAN = registerBiomeKey("deep_stormy_lukewarm_ocean");
     public static final ResourceKey<Biome> PRIMORDIAL_ISLAND = registerBiomeKey("primordial_island");
     public static final ResourceKey<Biome> PRIMORDIAL_OCEAN = registerBiomeKey("primordial_ocean");
     public static final ResourceKey<Biome> DEEP_PRIMORDIAL_OCEAN = registerBiomeKey("deep_primordial_ocean");
-    public static final ResourceKey<Biome> NULL_BIOME = registerBiomeKey("null");
 
     public static void bootstrap(BootstrapContext<Biome> context) {
         context.register(PRIMORDIAL_ISLAND, createPrimordialIslandBiome(context));
-        context.register(PRIMORDIAL_OCEAN, createPrimordialOceanBiome(context, false));
-        context.register(DEEP_PRIMORDIAL_OCEAN, createDeepPrimordialOceanBiome(context, true));
-        context.register(NULL_BIOME, createNullBiome(context));
+        context.register(PRIMORDIAL_OCEAN, createPrimordialOceanBiome(context));
+        context.register(DEEP_PRIMORDIAL_OCEAN, createDeepPrimordialOceanBiome(context));
+        context.register(DEEP_STORMY_LUKEWARM_OCEAN, createStormBiome(context));
     }
 
-    private static Biome createNullBiome(BootstrapContext<Biome> context) {
+    private static Biome createStormBiome(BootstrapContext<Biome> context) {
         MobSpawnSettings.Builder spawns = new MobSpawnSettings.Builder();
+            BiomeDefaultFeatures.warmOceanSpawns(spawns, 1, 1);
+            spawns.addSpawn(MobCategory.WATER_AMBIENT, 10, new MobSpawnSettings.SpawnerData(EntityType.COD, 3, 6));
+            BiomeDefaultFeatures.monsters(spawns, 95, 5, 100, 8, false);
         BiomeGenerationSettings.Builder geo = new BiomeGenerationSettings.Builder(
                 context.lookup(Registries.PLACED_FEATURE),
                 context.lookup(Registries.CONFIGURED_CARVER));
+        BiomeDefaultFeatures.addDefaultCarversAndLakes(geo);
+        BiomeDefaultFeatures.addDefaultOres(geo);
+        BiomeDefaultFeatures.addDefaultSoftDisks(geo);
+        BiomeDefaultFeatures.addLukeWarmKelp(geo);
         return new Biome.BiomeBuilder()
                 .hasPrecipitation(false)
                 .temperature(0.5F)
-                .downfall(0.0F)
+                .downfall(0.5F)
                 .mobSpawnSettings(spawns.build())
                 .generationSettings(geo.build())
                 .specialEffects(new BiomeSpecialEffects.Builder()
-                        .waterColor(0x000000)
-                        .grassColorOverride(0x000000)
-                        .foliageColorOverride(0x000000)
+                        .waterColor(0x45AD12)
+                        .grassColorOverride(0x04131A)
+                        .foliageColorOverride(0x71A74D)
                         .grassColorModifier(BiomeSpecialEffects.GrassColorModifier.NONE)
                         .build())
                 .build();
@@ -75,7 +88,7 @@ public class ModBiomes {
                 .build();
     }
 
-    private static Biome createPrimordialOceanBiome(BootstrapContext<Biome> context, boolean isDeep) {
+    private static Biome createPrimordialOceanBiome(BootstrapContext<Biome> context) {
         MobSpawnSettings.Builder spawns = new MobSpawnSettings.Builder();
         BiomeGenerationSettings.Builder geo = new BiomeGenerationSettings.Builder(
                 context.lookup(Registries.PLACED_FEATURE),
@@ -90,13 +103,13 @@ public class ModBiomes {
                 .mobSpawnSettings(spawns.build())
                 .generationSettings(geo.build())
                 .specialEffects(new BiomeSpecialEffects.Builder()
-                        .waterColor(isDeep ? 0x1A237E : 0x19B6F6)
+                        .waterColor(0x1A237E)
                         .grassColorModifier(BiomeSpecialEffects.GrassColorModifier.DARK_FOREST)
                         .build())
                 .build();
     }
 
-    private static Biome createDeepPrimordialOceanBiome(BootstrapContext<Biome> context, boolean isDeep) {
+    private static Biome createDeepPrimordialOceanBiome(BootstrapContext<Biome> context) {
         MobSpawnSettings.Builder spawns = new MobSpawnSettings.Builder();
         BiomeGenerationSettings.Builder geo = new BiomeGenerationSettings.Builder(
                 context.lookup(Registries.PLACED_FEATURE),
